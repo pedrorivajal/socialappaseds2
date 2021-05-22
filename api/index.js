c838c04e8615824cf4352544482ec3422fc222ce
pedrorivajal <rivajalpedro@example.com>
2021-05-22 19:09:19 +0000

    first commit

---
+++ api/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
+const router = express.Router();
+const path = require("path");
+
+dotenv.config();
+
+const db = require('./keys').MONGO_URL;
+
+mongoose.connect(db).then( () =>console.log("MongoDb connectee")).catch ( err => console.log(err));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(8800, () => {
+  console.log("serveur backend est en cours d'exécution sur le port 8800");
+});
---
+++ api/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URL : 'mongodb+srv://houssam:houssamelhazami@cluster0.qvssv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+}
